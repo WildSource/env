8c4c79d5740f2f48479fc8d17b05f3244a705873
dy/dx <ilovetrap123@hotmail.com>
2024-11-28 09:08:27 -0500

    loadEnvFile with path works

--- src/Env.hs
+++ src/Env.hs
@@ -2,7 +2,8 @@
 
 module Env where
 
-import Data.Text (Text, splitOn)
+import Data.Text (Text, splitOn, pack, unpack)
+import System.Environment (setEnv)
 
 type Key = Text 
 type Value = Text 
@@ -10,8 +11,20 @@ type Value = Text
 -- | Load the env file specified on the path 
 loadEnvFile :: String -> IO ()
 loadEnvFile path = 
-  let content = readFile path 
-  in putStrLn $ content >>= lines 
+  let content = (readFile path) >>= (return . lines)
+  in content >>= loadEnvFile'
+    where
+    loadEnvFile' :: [String] -> IO () 
+    loadEnvFile' [] = pure ()  
+    loadEnvFile' [x] = loadEnvValue x
+    loadEnvFile' (x:xs) = 
+      loadEnvValue x >> loadEnvFile' xs
+
+    loadEnvValue :: String -> IO ()
+    loadEnvValue rawData = 
+      let (k, v) = mapTuple (parse $ pack rawData) (unpack)
+      in setEnv k v
+
 -- | Load the the first found env file
 loadEnv :: IO ()
 loadEnv = undefined
@@ -24,3 +37,6 @@ parse lexime =
     genKV :: [Text] -> (Key, Value)
     genKV [h, t] = (h, t)
     genKV _ = ("","")
+
+mapTuple :: (a,a) -> (a -> b) -> (b,b)
+mapTuple (v, v') f = (f v, f v')
